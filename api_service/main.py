660668f221b617988fdec80539580031ab6138dc
Dinesh Yadav <dineshnirban01@gmail.com>
2026-07-27 23:15:47 +0530

    fixed chat router

--- api_service/main.py
+++ api_service/main.py
@@ -49,7 +49,7 @@ app.add_middleware(
     allow_origins=_cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"],  
 )
 
 # Exposes GET /metrics (request count/latency/status codes, broken down by
